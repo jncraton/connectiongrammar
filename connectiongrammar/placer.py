ad9ba000047a35195279464d1b165e68ea1e2367
Jon Craton <jncraton@gmail.com>
2018-03-19 14:54:31 -0400

    Rotation matricies

--- connectiongrammar/placer.py
+++ connectiongrammar/placer.py
@@ -22,6 +22,31 @@ import connectiongrammar
 
 OP = enum.Enum('OP', 'Place Move FillRect Rotate ( )')
 
+def rotation_matrix(dir, ldraw_string=False):
+  """ Gets a rotation matrix from a simple cardinal direction
+  >>> rotation_matrix(0)
+  [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+  >>> rotation_matrix(2)
+  [[-1, 0, 0], [0, 1, 0], [0, 0, -1]]
+  """
+  matricies = [ #front,right,back,left
+    "1 0 0 0 1 0 0 0 1", #identity matrix
+    "0 0 -1 0 1 0 1 0 0",
+    "-1 0 0 0 1 0 0 0 -1",
+    "0 0 1 0 1 0 -1 0 0",
+  ]
+
+  if ldraw_string:
+    return matricies[dir]
+  else:
+    items = [int(i) for i in matricies[dir].split()]
+    return [
+      items[0:3],
+      items[3:6],
+      items[6:9],
+    ]
+
+
 @functools.lru_cache()
 def get_token(lexeme):
   """ Convert lexems to (name, arg) pairs 
@@ -64,14 +89,38 @@ def lex(text):
   
   return map(get_token, text)
 
+def apply_rotation(v, mat):
+  """ Applies a rotation matrix to a vector 
+
+  >>> apply_rotation([1,2,3],[[1,0,0], [0,1,0], [0,0,1]])
+  [1, 2, 3]
+  >>> apply_rotation([1,2,3],[[0,0,-1], [0,1,0], [1,0,0]])
+  [-3, 2, 1]
+  """
+  if isinstance(mat, int):
+    mat = rotation_matrix(mat)
+  
+  return [mat[i][0] * v[0] +\
+          mat[i][1] * v[1] +\
+          mat[i][2] * v[2] for i in range(0, len(v))]
+
 def move(s, delta):
   """
-  >>> move((0, 0, 0, 1), (2, 2, 2))
-  (2, 2, 2, 1)
-  >>> move((0, 0, 0, -1), (2, 2, 2))
-  (-2, 2, -2, -1)
+  >>> move((0, 0, 0, 0), (1, 2, 3))
+  (1, 2, 3, 0)
+  >>> move((0, 0, 0, 1), (1, 2, 3))
+  (-3, 2, 1, 1)
+  >>> move((0, 0, 0, 2), (1, 2, 3))
+  (-1, 2, -3, 2)
+  >>> move((0, 0, 0, 3), (1, 2, 3))
+  (3, 2, -1, 3)
   """
-  return (s[0]+delta[0]*s[3], s[1]+delta[1], s[2]+delta[2]*s[3],s[3])
+
+  rot = rotation_matrix(s[3])
+
+  rot_delta = apply_rotation(delta, rot)
+
+  return (s[0]+rot_delta[0], s[1]+rot_delta[1], s[2]+rot_delta[2],s[3])
 
 class CollisionError(BaseException): pass
 
@@ -87,11 +136,13 @@ class VolumetricImage:
     This operation can be viewed as one transaction. If it fails for 
     any point, no changes are made.
     """
+    size = [abs(i) for i in apply_rotation(size,pos[3])]
+    
     new_pos = []
-  
+
     for x in range(-int(size[0]/2), int(size[0]/2)):
       for y in range(0, size[1]):
-        for z in range(-int(size[0]/2), int(size[0]/2)):
+        for z in range(-int(size[2]/2), int(size[2]/2)):
           new_pos.append((pos[0] + x, pos[1] + y, pos[2] + z))
           if new_pos[-1] in self.voxels:
             raise CollisionError('Cannot fill %s' % (new_pos[-1],))
@@ -113,13 +164,13 @@ def bounding_sphere(r, b):
 def parse(text, start_voxels=None):
   """ Returns the volumetric image for a text 
   >>> parse("FillRect(2,3,2) Place(3005)")[0]
-  [((0, 0, 0, 1), 1, '3005')]
+  [((0, 0, 0, 0), 1, '3005')]
   >>> len(parse("FillRect(2,3,2) Place(3005)")[1].voxels)
   2689
   """
   img = VolumetricImage(start_voxels or bounding_sphere(7, 1))
 
-  state = (0,0,0,1)
+  state = (0,0,0,0)
   states = []
   elements = []
 
@@ -135,7 +186,7 @@ def parse(text, start_voxels=None):
     elif op[0] == OP.Move:
       state = move(state, op[1])
     elif op[0] == OP.Rotate:
-      state = (state[0],state[1],state[2],state[3] * -1)
+      state = (state[0],state[1],state[2],(state[3] + int(op[1]/90)) % 4)
     elif op[0] == OP.FillRect:
       img.fill_rect(state, op[1])
     else:
@@ -144,10 +195,6 @@ def parse(text, start_voxels=None):
   return (elements, img)
 
 def to_ldraw(els):
-  front = "1 0 0 0 1 0 0 0 1"
-  back = "-1 0 0 0 1 0 0 0 -1"
-  left = "0 0 1 0 1 0 -1 0 0"
-  right = "0 0 -1 0 1 0 1 0 0"
   color = 0
   yellow = 14
   blue = 1
@@ -157,11 +204,9 @@ def to_ldraw(els):
   for el in els:
     pos = (el[0][0] * 10, el[0][1] * 8, el[0][2] * 10)
 
-    facing = front
-    if el[2][-1] == 'r':
-      facing = right
+    rotation = rotation_matrix(el[0][3], ldraw_string=True)
 
-    ldraw += ("1 %d %d %d %d %s %s.dat\n" % (el[1], pos[0], pos[1], pos[2], facing, el[2].replace('r','')))
+    ldraw += ("1 %d %d %d %d %s %s.dat\n" % (el[1], pos[0], pos[1], pos[2], rotation, el[2].replace('r','')))
     ldraw += "0 STEP\n"
 
   return ldraw
