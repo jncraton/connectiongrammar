a99b174d443d3c95c7aa8f6e834323a3e571669a
Jon Craton <jncraton@gmail.com>
2018-03-26 22:57:11 -0400

    Converted to probabilistic grammars

--- run_grammars.py
+++ run_grammars.py
@@ -3,15 +3,32 @@ import sys
 
 from connectiongrammar import connectiongrammar, placer
 
-from nltk import CFG
+from nltk import PCFG
 
 gmr_text = ""
 
 for grammar_file in sys.argv[1:]:
   gmr_text += open('grammars/%s.gmr' % grammar_file).read()
 
+sym_count = {}
+
+def get_sym(line):
+  return line.split('-')[0].strip()
+
+for line in gmr_text.splitlines():
+  if line and not line.endswith(']'):
+    sym_count[get_sym(line)] = sym_count.get(get_sym(line), 0) + 1
+
+def add_prob(line):
+  if not line or line.endswith(']'):
+    return line
+
+  return '%s [%f]' % (line, 1 / sym_count[get_sym(line)])
+
+gmr_text = '\n'.join(map(add_prob, gmr_text.splitlines()))
+
 cg = connectiongrammar.ConnectionGrammar(
-  grammar = CFG.fromstring(gmr_text),
+  grammar = PCFG.fromstring(gmr_text),
   fitness = placer.fitness
 )
 
